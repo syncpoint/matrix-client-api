6a2192cc2b51a9592e81d7d1c5a8cc45b9f55cff
Thomas Halwax <thomas.halwax@syncpoint.io>
2023-03-09 18:38:28 +0100

    auto retry

--- src/timeline-api.mjs
+++ src/timeline-api.mjs
@@ -76,17 +76,37 @@ TimelineAPI.prototype.catchUp = async function (roomId, lastKnownStreamToken, cu
 
 
 TimelineAPI.prototype.stream = async function* (since, filter, signal = (new AbortController()).signal) {
-  let streamToken
+  let streamToken = since
   
   const initialSync = await this.syncTimeline(since, filter, 0)
   streamToken = initialSync.next_batch
   yield initialSync
-    
+
+  const delay = value => new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, value);
+  })
+
+  let requestCounter = 1
+  let coolOffTime = 0 // time in ms to wait before next try, changed in case of an error
   while (!signal.aborted) {
+    try {
+      await delay(coolOffTime)
       const syncResult = await this.syncTimeline(streamToken, filter, DEFAULT_POLL_TIMEOUT, signal)
-      if (syncResult.next_batch === streamToken) continue // no new events
-      streamToken = syncResult.next_batch
-      yield syncResult
+      coolOffTime = 0
+      if (streamToken !== syncResult.next_batch) {
+        console.log('streamToken changed to ', syncResult.next_batch)
+        streamToken = syncResult.next_batch
+        console.log('This was request # ', requestCounter)
+        requestCounter++
+        yield syncResult
+      }
+    } catch (error) {
+      coolOffTime = 30_000
+      yield new Error('probably offline')
+    }
+      
   }
 }
 
