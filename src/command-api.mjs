df8ac6c70614d92dee8f9df91bbfbe0b6f1fd838
Thomas Halwax <thomas.halwax@syncpoint.io>
2023-03-20 15:30:14 +0100

    much better queueing

--- src/command-api.mjs
+++ src/command-api.mjs
@@ -1,11 +1,9 @@
 import { FIFO } from './queue.mjs'
 
 class CommandAPI {
-  constructor (httpAPI, retryInterval = 30000) {
+  constructor (httpAPI) {
     this.httpAPI = httpAPI
-    this.retryInterval = retryInterval
     this.scheduledCalls = new FIFO()
-    this.execute()
   }
 
   /**
@@ -22,24 +20,57 @@ class CommandAPI {
     this.scheduledCalls.enqueue(functionCall)
   }
 
-  async execute () {
+
+  async run () {
+
+      /**
+   * @param {Number} retryCounter 
+   * @returns A promise that resolves after a calculated time depending on the retryCounter using an exponential back-off algorithm. The max. delay is 30s.
+   */
+    const chill = retryCounter => new Promise(resolve => {
+      const BACKOFF_FACTOR = 0.5
+      const BACKOFF_LIMIT = 30_000
+      const delay = Math.min(BACKOFF_LIMIT, (retryCounter === 0 ? 0 : BACKOFF_FACTOR * (2 ** (retryCounter)) * 1000))
+      setTimeout(() => {
+        resolve()
+      }, delay)
+    })
+
+    if (this.controller) return
+    this.controller = new AbortController()
+    
+    let retryCounter = 0
     let functionCall
-    try {
-      functionCall = await this.scheduledCalls.dequeue()
-      const [functionName, ...params] = functionCall
-      await this.httpAPI[functionName].apply(this.httpAPI, params)
-      setImmediate(() => this.execute())
-      console.log(`Called ${functionName}`)
-    } catch (error) {
-      if (error.response?.statusCode === 403) {
-        console.error(`Calling ${functionCall[0]} is forbidden: ${error.response.body}`)
-        return setImmediate(() => this.execute())
+
+    while (!this.controller.signal.aborted) {
+      try {
+        await chill(retryCounter)
+
+        functionCall = await this.scheduledCalls.dequeue()
+        const [functionName, ...params] = functionCall        
+        await this.httpAPI[functionName].apply(this.httpAPI, params)
+        console.log('SUCCESS', functionName, params)
+        retryCounter = 0
+      } catch (error) {
+        console.log('ERROR', error.message)
+        if (error.response?.statusCode === 403) {
+          console.error(`Calling ${functionCall[0]} is forbidden: ${error.response.body}`)
+        }
+        
+        /*
+          In most cases we will have to deal with socket errors. The users computer may
+          be offline or the server might be unreachable.
+        */
+        console.log(`Error: ${error.message}`)
+        this.scheduledCalls.requeue(functionCall)
+        retryCounter++
       }
-      
-      console.log(`Error: ${error.message}`)
-      this.scheduledCalls.requeue(functionCall)
-      setTimeout(() => this.execute(), this.retryInterval)
-    }
+    }    
+  }
+
+  async stop () {
+    this.controller?.abort()
+    delete this.controller
   }
 }
 
