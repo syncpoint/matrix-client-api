c0d36a683e5b947f6890470363c4ab8560d37c68
Thomas Halwax <thomas.halwax@syncpoint.io>
2023-06-28 16:33:38 +0200

    fixed sharing the same project multiple times

--- src/project-list.mjs
+++ src/project-list.mjs
@@ -33,6 +33,10 @@ ProjectList.prototype.hydrate = async function () {
 }
 
 ProjectList.prototype.share = async function (projectId, name, description) {
+  if (this.wellKnown.get(projectId)) {
+    /* project is already shared */
+    return
+  }
   const result = await this.structureAPI.createProject(projectId, name, description)
   this.wellKnown.set(result.globalId, result.localId)
   this.wellKnown.set(result.localId, result.globalId)
